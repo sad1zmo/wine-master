b864a15e593c2aa51701073a69e3d33c3860c3be
Sergei <suz2k@yandex.ru>
2024-04-11 06:40:31 +0400

    fixed all

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from support_functions import get_company_years, \
-                              single_or_plural, \
+                              is_year_single_or_plural, \
                               get_information_from_exel, \
-                              get_formated_data
+                              get_formated_drinks_data
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from environs import Env
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def main():
+    env = Env()
+    exel_filepath = env.str('EXEL_FILEPATH', default='wine.xlsx')
+    exel_sheet_name = env.str('EXEL_SHEET_NAME', default='Лист1')
 
-rendered_page = template.render(
-    years=get_company_years(),
-    single_or_plural=single_or_plural(get_company_years()),
-    wines_info=get_formated_data(
-        get_information_from_exel('wine.xlsx', 'Лист1')),
-    categories=get_formated_data(
-        get_information_from_exel('wine.xlsx', 'Лист1')).keys()
+    jinja_env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
     )
 
+    template = jinja_env.get_template('template.html')
+    formated_drinks_info = get_formated_drinks_data(
+            get_information_from_exel(exel_filepath, exel_sheet_name))
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    rendered_page = template.render(
+        years=get_company_years(),
+        single_or_plural=is_year_single_or_plural(get_company_years()),
+        wines_date_for_template=formated_drinks_info,
+        )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding='utf8') as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
--- support_functions.py
+++ support_functions.py
@@ -9,7 +9,7 @@ def get_company_years():
     return now_year - open_years
 
 
-def single_or_plural(years):
+def is_year_single_or_plural(years):
     if years % 10 == 1 and years % 100 != 11:
         return "год"
     elif 2 <= years % 10 <= 4 and (years % 100 < 10 or years % 100 >= 20):
@@ -18,17 +18,17 @@ def single_or_plural(years):
         return "лет"
 
 
-def get_information_from_exel(file, sheet_name):
-    excel_data_df = pandas.read_excel(file, sheet_name=sheet_name,
+def get_information_from_exel(filepath, sheet_name):
+    excel_data_df = pandas.read_excel(filepath, sheet_name=sheet_name,
                                       na_values=[' ', 'N/A', 'NA'],
                                       keep_default_na=False)
-    list_of_dicts = excel_data_df.to_dict(orient='records')
-    return list_of_dicts
+    drink_records = excel_data_df.to_dict(orient='records')
+    return drink_records
 
 
-def get_formated_data(list_of_dicts):
-    wine_dict = collections.defaultdict(list)
-    for wine_info in list_of_dicts:
-        category = wine_info['Категория']
-        wine_dict[category].append(wine_info)
-    return wine_dict
+def get_formated_drinks_data(records_about_drinks):
+    formated_drinks_info = collections.defaultdict(list)
+    for record_describe in records_about_drinks:
+        category = record_describe['Категория']
+        formated_drinks_info[category].append(record_describe)
+    return formated_drinks_info
